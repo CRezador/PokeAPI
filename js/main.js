36d24aece3ca3bf439135de14d6dd8a2b8248c41
Caio Rezador <caio.rezador@gmail.com>
2022-06-12 20:26:43 -0300

    Trabalhando no Layout para Desktop

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 //Chamando a API
-var id = 1
+let id = 1;
 
 //Funções Assíncronas
 //Chamando a API pela Geração
-async function getPokemonIdByGeneretion(id) {
+async function getPokemonIdByGeneretion() {
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/generation/${id}`)
-    const data = await response.json()
-    return data.pokemon_species
+    const pokemon = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?limit=9&offset=2`
+    ).then(response => {
+      return response.json()
+    })
+    return pokemon.results
   } catch (error) {
     console.error(error)
   }
 }
 
-async function getAllPokemon(pokemon) {
+async function getAllPokemon(pokemonId) {
   try {
     const responsePokemon = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
-    )
-    var data = responsePokemon.json()
-    return data
+      `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`
+    ).then(response => {
+      return response.json()
+    })
+    return responsePokemon
   } catch (error) {
     console.error(error)
   }
@@ -27,51 +31,88 @@ async function getAllPokemon(pokemon) {
 
 //fuctions
 function getTypePokemon(pokemonTypes) {
-  return pokemonTypes
-    .map(typeInfo => {
-      let type = typeInfo.type.name
-      return type
-    })
-    .join(' | ')
+  let acc;
+  pokemonTypes.forEach( (typeInfo) => { 
+    acc == null ?(
+      acc = `<div class="type-pokemon ${typeInfo.type.name}">${typeInfo.type.name}</div>`
+    ) :
+    (
+      acc += `<div class="type-pokemon ${typeInfo.type.name}">${typeInfo.type.name}</div>`
+    )
+  });
+  return acc;
 }
+
 
 function getAbilitiesPokemon(abilities) {
   return abilities.reduce((acc, abilityInfo) => {
-    acc += `<li> ${abilityInfo.ability.name}</li>`
+    acc += `<span class="ability"> ${abilityInfo.ability.name}</span>`
     return acc
   }, '')
 }
 
 function getStatsPokemon(stats) {
   return stats.reduce((acc, statsInfo) => {
-    acc += `<li>${statsInfo.stat.name}: ${statsInfo.base_stat}</li>`
+    if(statsInfo.stat.name == 'attack' || statsInfo.stat.name == 'defense' )
+    acc += ` <div>
+                <div class="stat-base">
+                  <span>${statsInfo.base_stat}</span>
+                </div>
+                <div class="stat-name">
+                  <span>${statsInfo.stat.name}</span>
+                </div>  
+              </div>`
     return acc
   }, '')
 }
-const PokemonId = getPokemonIdByGeneretion(id)
 
-PokemonId.then(values => {
-  var pokemons = values.map(pokemonsID => {
-    let pokemon = pokemonsID.name
-    return getAllPokemon(pokemon)
-  })
+function getImage(id) {
+  let image
+  if (id > 99) {
+    image = id
+  } else if (id > 9) {
+    image = '0' + id
+  } else {
+    image = '00' + id
+  }
 
-  Promise.all(pokemons).then(pokemonList => {
-    const main = document.querySelector('main')
-    const pokemon = pokemonList.reduce((accumulator, pokemon) => {
-      accumulator += `
-      <img src="${pokemon.sprites.front_default}" alt="">
-      <p>ID: ${pokemon.id} </p>
-      <p>Pokemon: ${pokemon.name} </p>
-      <p>Type: ${getTypePokemon(pokemon.types)} </p>
-      <p>Habilidades: </p>
-      <ul>${getAbilitiesPokemon(pokemon.abilities)}</ul>
-      <p>Stats</p>
-      <ul>${getStatsPokemon(pokemon.stats)}</ul>
-      <br/>`
-      return accumulator
-    }, '')
+  return image
+}
 
-    main.innerHTML = pokemon
-  })
-})
+async function createPokemon(PokemonId) {
+  try {
+    await PokemonId.then(values => {
+      let pokemons = values.map(pokemonsID => {
+        let pokemon = pokemonsID.name
+        return getAllPokemon(pokemon)
+      })
+
+      Promise.all(pokemons).then(pokemonList => {
+        const run = document.querySelector('.pokemon-grid');
+        const pokemon = pokemonList.reduce((accumulator, pokemon) => {
+          accumulator += `
+        <div class="pokemon-card">  
+          <div class="pokemon-image ${pokemon.types[0].type.name}">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${getImage(pokemon.id)}.png" alt="Foto do pokemons ${pokemon.name}">
+          </div>
+          <div class="pokemons-data">
+            <h5 class="pokemon-title">${pokemon.name}</h5>
+            <div class="pokemon-stats">${getStatsPokemon(pokemon.stats)}</div>
+            <div class="pokemon-types">${getTypePokemon(pokemon.types)}</div>
+          </div>
+        </div>
+        `
+          return accumulator
+        }, '')
+        
+        
+        run.innerHTML = pokemon
+      })
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+const PokemonId = getPokemonIdByGeneretion()
+
+createPokemon(PokemonId)
